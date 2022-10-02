4ca1b3b2670494fcfdcd211bedfa6da3f5ae1877
root <gotfede@gmail.com>
2022-10-02 22:06:08 +0100

    Scraping and saving in csv

--- prova.py
+++ prova.py
@@ -1,7 +1,25 @@
-import requests  #IMPORT REQUESTS
+import requests
+from bs4 import BeautifulSoup as bs
+import csv
 
-r = requests.get('http://www.geeksforgeeks.org/python-programming-language/')
+URL = 'https://www.geeksforgeeks.org/page/'
 
-print(r)
+soup = bs(req.text, 'html.parser')
 
-print(r._content)
+titles = soup.find_all('div', attrs={'class', 'head'})
+titles_list = []
+
+count = 1
+for title in titles:
+	d = {}
+	d['Title Number'] = f'Title {count}'
+	d['Title Name'] = title.text
+	count += 1
+	titles_list.append(d)
+
+filename = 'titles.csv'
+with open(filename, 'w', newline='') as f:
+	w = csv.DictWriter(f,['Title Number','Title Name'])
+	w.writeheader()
+	
+	w.writerows(titles_list)
